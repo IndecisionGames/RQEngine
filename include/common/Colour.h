486a168bf0b40f33968927d2fb3cb1f408f9ce04
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2021-03-24 22:05:02 +0000

    Add colour header

---
+++ include/common/Colour.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+
+glm::vec3 BLACK = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 WHITE = glm::vec3(1.0f, 1.0f, 1.0f);
+
+glm::vec3 RED = glm::vec3(1.0f, 0.0f, 0.0f);
+glm::vec3 GREEN = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 BLUE = glm::vec3(0.0f, 0.0f, 1.0f);
+
+glm::vec3 YELLOW = glm::vec3(1.0f, 1.0f, 0.0f);
+glm::vec3 CYAN = glm::vec3(0.0f, 1.0f, 1.0f);
+glm::vec3 MAGENTA = glm::vec3(1.0f, 0.0f, 1.0f);
